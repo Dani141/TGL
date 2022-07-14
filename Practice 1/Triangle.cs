5c6e6cf4942e44895219ecff204d7e9a26c3704d
Daniela <83104187+Dani141@users.noreply.github.com>
2022-07-14 00:34:11 -0400

    TGL

---
+++ Practice 1/Triangle.cs
@@ -0,0 +1,21 @@
+
+public class Triangle: Figure{
+    
+    public double A {get;}
+    public double B {get;}
+    public double C {get;}
+
+
+    public Rectangle(double a, double b, double c){
+      A=a;
+      B=b;
+      C=c;
+    }
+    public override double CalculateArea(){
+        var p = CalculatePerimeter() / 2;
+        return  Math.Sqrt(p*(p-A)*(p-B)*(p-C));
+    }
+    public override double CalculatePerimeter(){
+        return A+B+C;
+    }
+}
